70aecdb5fa66de0fe17b7a130a85fb503ac4d707
Michael Schennum <mikeschenphoto@gmail.com>
2016-06-04 17:37:17 -0700

    adds Obsevable for user name

--- app/src/main/java/com/mikeschen/www/rxsample/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/rxsample/MainActivity.java
@@ -2,6 +2,12 @@ package com.mikeschen.www.rxsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import rx.Observable;
+import rx.android.widget.OnTextChangeEvent;
+import rx.android.widget.WidgetObservable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        Observable<OnTextChangeEvent> userNameText =
+                WidgetObservable.text((EditText) findViewById(R.id.edtUserName));
+
+        userNameText.subscribe( e -> Log.d("[Rx]", e.text().toString()));
     }
 }
